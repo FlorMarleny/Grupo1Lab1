fd15757ee404da92093dd4fd122819e01778e71c
Marleny <flor.rodriguez@tecsup.edu.pe>
2026-04-02 05:31:00 +0200

    FMRB. Nombre de cliente y Nombre de 5 productos.

--- Grupo1Lab1/Program.cs
+++ Grupo1Lab1/Program.cs
@@ -1,13 +1,21 @@
-﻿Console.WriteLine("Hello, Grupo2");
-Console.WriteLine("Hello, Manuel ");
-Console.WriteLine("Hello, Marle ");
-Console.WriteLine("hello gary");
-Console.WriteLine("hello diego");
-
-// Variables
+﻿// Variables
 string nombreCliente;
 string[] productos = new string[5];
 int[] cantidades = new int[5];
 double[] precios = new double[5];
 double[] subtotales = new double[5];
 double totalGeneral = 0;
+
+// Nombre del cliente
+Console.Write("Ingrese nombre del cliente: ");
+nombreCliente = Console.ReadLine();
+
+// Nombre de 5 productos
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"\nProducto {i + 1}");
+
+    Console.Write("Nombre del producto: ");
+    productos[i] = Console.ReadLine();
+
+}
