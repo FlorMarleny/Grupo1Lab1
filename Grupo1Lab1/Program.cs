f2641e5290b8218bcfc99a143cddc80c73d6e0e1
Diexmania1992 <140257197+Diexmania1992@users.noreply.github.com>
2026-04-01 22:17:51 -0500

    variables.diego

--- Grupo1Lab1/Program.cs
+++ Grupo1Lab1/Program.cs
@@ -3,3 +3,11 @@ Console.WriteLine("Hello, Manuel ");
 Console.WriteLine("Hello, Marle ");
 Console.WriteLine("hello gary");
 Console.WriteLine("hello diego");
+
+// Variables
+string nombreCliente;
+string[] productos = new string[5];
+int[] cantidades = new int[5];
+double[] precios = new double[5];
+double[] subtotales = new double[5];
+double totalGeneral = 0;
